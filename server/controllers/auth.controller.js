ea5b37ad204601aa88f160a4a3193adec65a60e9
arthdarji352 <arthdarji20@gmail.com>
2024-03-03 22:41:54 +0530

    add login route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { registerUser } from "../services/auth.service.js";
+import { LoginUser, registerUser } from "../services/auth.service.js";
 
 //Register
 export const register = async (req, res) => {
@@ -14,6 +14,26 @@ export const register = async (req, res) => {
     res.status(500).json({
       error: error,
       message: "Error Occurred Registering User",
+    });
+    console.log(error);
+  }
+};
+
+//login
+
+export const login = async (req, res) => {
+  try {
+    const user = await LoginUser(req.body, res);
+
+    const { password, ...data } = user._doc;
+    res.status(200).json({
+      data,
+      message: "User Login successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+      message: "Error Occurred Login User",
     });
     console.log(error);
   }
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,13 +1,13 @@
 import UserModel from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
-export const registerUser = async (body, res) => {
-  const { username, email, password } = body;
+export const registerUser = async (req, res) => {
+  const { username, email, password } = req;
 
   if (!username || !email || !password) {
-    return res.status(400).send("Please provile details");
+    return res.status(400).json("Please provile details");
   }
-  const hashedPassword = bcrypt.hashSync(body.password, 10);
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
   const newUser = new UserModel({
     username: username,
@@ -18,4 +18,23 @@ export const registerUser = async (body, res) => {
   await newUser.save();
 
   return newUser;
+};
+
+export const LoginUser = async (req, res) => {
+  const { email, password } = req;
+
+  if (!email || !password) {
+    return res.status(400).json("Please provile details");
+  }
+
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  const passwordCheck = await bcrypt.compare(password, user.password);
+  if (!passwordCheck) {
+    return res.status(400).json("wrong password");
+  }
+  return user;
 };
